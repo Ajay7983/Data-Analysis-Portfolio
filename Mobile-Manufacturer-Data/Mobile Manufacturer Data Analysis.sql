abd8ca7c8cd6324ba4e81556c99d7c0400008c76
Ajay <110324440+Ajay7983@users.noreply.github.com>
2024-12-19 13:47:13 +0530

    Update Mobile Manufacturer Data Analysis.sql

--- Mobile-Manufacturer-Data/Mobile Manufacturer Data Analysis.sql
+++ Mobile-Manufacturer-Data/Mobile Manufacturer Data Analysis.sql
@@ -1,4 +1,3 @@
---SQL Advance Case Study
 USE db_SQLCaseStudies
 
 --Q1--BEGIN 
@@ -8,7 +7,6 @@ LEFT JOIN DIM_DATE  DT ON TR.Date= DT.DATE
 LEFT JOIN DIM_LOCATION  LC ON TR.IDLocation= LC.IDLocation
 WHERE DT.YEAR >2005
 GROUP BY LC.State
-
 --Q1--END
 
 --Q2--BEGIN
@@ -26,33 +24,24 @@ WHERE MF.Manufacturer_Name = 'Samsung'
 T1.IDModel = T2.IDModel
 GROUP BY T1.State
 ORDER BY TOTAL_SALE DESC
-
-
 --Q2--END
 
 --Q3--BEGIN      
-	
 SELECT LC.State, LC.ZipCode, ML.Model_Name, COUNT(FT.IDCustomer) AS TOTAL_TRANS
 FROM FACT_TRANSACTIONS FT
 LEFT JOIN DIM_LOCATION LC ON FT.IDLocation = LC.IDLocation
 LEFT JOIN DIM_MODEL ML ON FT.IDModel = ML.IDModel
 GROUP BY LC.State, LC.ZipCode, ML.Model_Name
-
-
 --Q3--END
 
 --Q4--BEGIN
-
 SELECT TOP 1 DM.Model_Name, FT.TotalPrice
 FROM FACT_TRANSACTIONS FT
 JOIN DIM_MODEL DM ON FT.IDModel = DM.IDModel
 ORDER BY FT.TotalPrice;
-
-
 --Q4--END
 
 --Q5--BEGIN
-
 WITH Top5Manufacturers AS (
   SELECT TOP 5 DMF.IDManufacturer, SUM(FT.Quantity) as Sales
   FROM Fact_Transactions FT
@@ -72,7 +61,6 @@ ORDER BY Average_Price ASC;
 --Q5--END
 
 --Q6--BEGIN
-
 SELECT T2.Customer_Name, AVG(T1.TotalPrice) AS AVERAGE_PRICE
 FROM FACT_TRANSACTIONS T1
 LEFT JOIN DIM_CUSTOMER T2 ON T1.IDCustomer= T2.IDCustomer
@@ -80,12 +68,9 @@ LEFT JOIN DIM_DATE T3 ON T1.Date = T3.DATE
 WHERE T3.YEAR = 2009 
 GROUP BY T2.Customer_Name
 HAVING AVG(T1.TotalPrice) >500
-
-
 --Q6--END
 	
 --Q7--BEGIN  
-	
 WITH Top5Models2008 AS (
   SELECT TOP 5 Dim_Model.Model_Name, SUM(Fact_Transactions.Quantity) as Sales
   FROM Fact_Transactions
@@ -119,7 +104,6 @@ SELECT Model_Name FROM Top5Models2010;
 
 
 --Q8--BEGIN
-
 WITH Sales2009 AS (
   SELECT Dim_Manufacturer.Manufacturer_Name, SUM(Fact_Transactions.Quantity) as Total_Sales
   FROM Fact_Transactions
@@ -152,7 +136,6 @@ WHERE RowNum = 2;
 
 
 --Q9--BEGIN
-	
 SELECT Dim_Manufacturer.Manufacturer_Name
 FROM Dim_Manufacturer
 WHERE Dim_Manufacturer.IDManufacturer IN (
@@ -171,7 +154,6 @@ WHERE Dim_Manufacturer.IDManufacturer IN (
 --Q9--END
 
 --Q10--BEGIN
-	
 WITH Customer_Spend AS (
   SELECT Dim_Customer.IDCustomer, Dim_Customer.Customer_Name,
     YEAR(Fact_Transactions.Date) AS Years, 
@@ -189,6 +171,5 @@ SELECT TOP 100 IDCustomer, Customer_Name, Years, Avg_Spend, Avg_Quantity,
   END AS Spend_Change_Percentage
 FROM Customer_Spend
 ORDER BY Avg_Spend DESC
-
 --Q10--END
 	
